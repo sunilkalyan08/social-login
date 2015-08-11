5c68fa4bf5b354f99b99ae947929d72629add8f9
sunil kalyan <sunilkalyan08@gmail.com>
2015-08-11 18:03:12 +0530

    Application with Social Login Button

---
+++ app/controllers/callbacks_controller.rb
@@ -0,0 +1,14 @@
+class CallbacksController < Devise::OmniauthCallbacksController
+    def facebook
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+    def google_oauth2
+    	@user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+    def linkedin
+    	@user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+end
