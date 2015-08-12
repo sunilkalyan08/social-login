4375dc49e019fea677939283ccc829822139c628
sunil kalyan <sunilkalyan08@gmail.com>
2015-08-12 11:10:19 +0530

    Removed Pry

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
      :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :linkedin]
      def self.from_omniauth(auth)
-     	binding.pry
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
