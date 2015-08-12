a126531d744756e26a501592a652153eed49d624
sunil kalyan <sunilkalyan08@gmail.com>
2015-08-12 16:22:12 +0530

    checking records with email rather than provider n uid

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-     :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :linkedin]
+         :recoverable, :rememberable, :trackable, 
+     :omniauthable
      def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      where(email: auth.info.email).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
         user.email = auth.info.email
